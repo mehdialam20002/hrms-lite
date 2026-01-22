d523420e976d5b72d65f2232d2334cc742238f1e
Mehdi Alam <mehdialam2002@gmail.com>
2026-01-22 16:42:10 +0530

    HRMS Lite full stack app

--- hrms-lite-frontend/hrms-lite-frontend/src/services/api.js
+++ hrms-lite-frontend/hrms-lite-frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://hrms-lite-backendd.onrender.com/api/v1",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
+
 
 export default api;
